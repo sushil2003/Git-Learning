3af0e1b5beaecc3944aebc37e3a345872c48d36b
Sushil Gayke <gaykesushil98@gmail.com>
2025-06-17 13:48:02 +0530

    Login Updated

--- first.js
+++ first.js
@@ -4,6 +4,7 @@ console.log(button);
 const login="Login page added";
 console.log(login);
 
+<<<<<<< HEAD
 const footer="Footer added in our website";
 console.log(footer);
 
@@ -12,3 +13,7 @@ console.log(footer);
 //Rebashing 
 //first go to the branch that you want to rebash
 //Then use - git rebase master
+=======
+const payment="Integrated the payment gateway";
+console.log(payment);
+>>>>>>> 4766925 (Login Added)
